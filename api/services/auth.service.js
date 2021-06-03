a0501eebf14ba32830926bd2e2fdafcf8c701aea
padma-pani-patra <padmapanipatra@gmail.com>
2021-06-03 20:32:18 +0530

    complete the api with promise

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -4,100 +4,103 @@ const { Auth, User } = require('../models')
 
 const AuthService = {
     async createAuthLog(token, userId) {
-        try {
-            // Authenticate the logs schema
-            const authLog = {
-                token: token,
-                _user_id: userId
+        return new Promise(async (resolve, reject) => {
+            try {
+                // Authenticate the logs schema
+                const authLog = {
+                    token: token,
+                    _user_id: userId
+                }
+
+                // Create the Auth logs
+                const auth = await Auth.create(authLog)
+
+                resolve(auth)
+            } catch (error) {
+                reject({ error: error })
             }
-
-            // Create the Auth logs
-            const auth = await Auth.create(authLog)
-
-            return auth
-        } catch (error) {
-            return { error: error }
-        }
+        })
     },
 
     async signIn(email, password) {
-        try {
-            // Check if User exists
-            const user = await User.findOne({ email: email })
+        return new Promise(async (resolve, reject) => {
+            try {
+                // Check if User exists
+                const user = await User.findOne({ email: email })
 
-            // If User isn't found
-            if (!user || user.active == false) {
-                return { error: 'Either user doesn\'t exist or was removed from the system' }
+                // If User isn't found
+                if (!user || user.active == false) {
+                    reject({ error: 'Either user doesn\'t exist or was removed from the system' })
+                }
+
+                //Decrypt the password
+                const decryptedPassword = await Password.decryptPassword(password, user.password)
+                // If the psasword is wrong
+                if (!decryptedPassword.password) {
+                    reject({ error: 'Please enter a valid password' })
+                }
+                // Create the signed token
+                const token = jwt.sign(user.toJSON(), "minor-project-2")
+                // Log the Auths
+                this.createAuthLog(token, user._id)
+
+                resolve({
+                    user: user,
+                    token: token
+                })
+
+            } catch (error) {
+                reject(error)
             }
-
-            //Decrypt the password
-            const decryptedPassword = await Password.decryptPassword(password, user.password)
-
-            // If the psasword is wrong
-            if (!decryptedPassword.password) {
-                return { error: 'Please enter a valid password' }
-            }
-
-            // Create the signed token
-            const token = jwt.sign(user.toJSON, 'minor-project-2')
-
-            // Log the Auths
-            this.createAuthLog(token, user._id)
-
-            return {
-                user: user,
-                token: token
-            }
-
-        } catch (error) {
-            return error
-        }
+        })
     },
     async signUp(userData) {
-        try {
-            // If the email already exists
-            const checkEmail = await User.findOne({ email: userData.email })
+        return new Promise(async (resolve, reject) => {
+            try {
+                // If the email already exists
+                const checkEmail = await User.findOne({ email: userData.email })
 
-            if (checkEmail) {
-                return { error: 'User with email already exists.' }
+                if (checkEmail) {
+                    reject({ error: 'User with email already exists.' })
+                }
+
+                // Encrypt the password
+                const encryptedPassword = await Password.encryptPassword(userData.password)
+
+                //If the password is wrong
+                if (!encryptedPassword) {
+                    reject({ error: 'Please choose a different password' })
+                }
+
+                // User Data
+                let data = {
+                    first_name: userData.first_name,
+                    last_name: userData.last_name,
+                    full_name: userData.first_name + ' ' + userData.last_name,
+                    email: userData.email,
+                    password: encryptedPassword.password
+                }
+                console.log(data);
+                // Create the new User
+                let user = await User.create(data)
+
+                // If user is not created
+                if (!user) {
+                    reject({ error: 'User was not created.' })
+                }
+
+                // SIgn In the current User 
+                let res = await this.signIn(userData.email, userData.password)
+
+                resolve({
+                    user: res.user,
+                    token: res.token
+                })
+
+            } catch (error) {
+                reject({ error: error })
             }
-
-            // Encrypt the password
-            const encryptedPassword = await Password.encryptPassword(userData.password)
-
-            //If the password is wrong
-            if (!encryptedPassword) {
-                return { error: 'Please choose a different password' }
-            }
-
-            // User Data
-            let data = {
-                first_name: userData.first_name,
-                last_name: userData.last_name,
-                fulll_name: userData.first_name + ' ' + userData.last_name,
-                email: userData.email,
-                password: encryptedPassword.password
-            }
-
-            // Create the new User
-            let user = await User.create(data)
-
-            // If user is not created
-            if (!user) {
-                return { error: 'User was not created.' }
-            }
-
-            // SIgn In the current User 
-            let res = await this.signIn(userData.email, userData.password)
-
-            return {
-                user: res.user,
-                token: res.token
-            }
-
-        } catch (error) {
-            return { error: error }
-        }
+        })
     }
 }
 
