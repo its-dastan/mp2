8df4baa2a007b15c7aba70a8f2a57d5d923d3960
padma-pani-patra <padmapanipatra@gmail.com>
2021-06-19 18:53:48 +0530

    added the changed

--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -23,23 +23,6 @@ const AuthService = {
     },
 
     async signIn(email, password) {
-<<<<<<< HEAD
-        try {
-            // Check if User exists
-            const user = await User.findOne({ email: email })
-            console.log(user);
-
-            // If User isn't found
-            if (!user || user.active == false) {
-                return { error: 'Either user doesn\'t exist or was removed from the system' }
-            }
-            //Decrypt the password
-            const decryptedPassword = await Password.decryptPassword(password, user.password)
-
-            // If the psasword is wrong
-            if (!decryptedPassword.password) {
-                return { error: 'Please enter a valid password' }
-=======
         return new Promise(async (resolve, reject) => {
             try {
                 // Check if User exists
@@ -68,7 +51,6 @@ const AuthService = {
 
             } catch (error) {
                 reject(error)
->>>>>>> promise
             }
         })
     },
@@ -118,47 +100,7 @@ const AuthService = {
             } catch (error) {
                 reject({ error: error })
             }
-<<<<<<< HEAD
-
-            // Encrypt the password
-            const encryptedPassword = await Password.encryptPassword(userData.password)
-
-            //If the password is wrong
-            if (!encryptedPassword) {
-                return { error: 'Please choose a different password' }
-            }
-
-            // User Data
-            let data = {
-                first_name: userData.first_name,
-                last_name: userData.last_name,
-                full_name: userData.first_name + ' ' + userData.last_name,
-                email: userData.email,
-                password: encryptedPassword.password
-            }
-
-            // Create the new User
-            let user = await User.create(data)
-
-            // If user is not created
-            if (!user) {
-                return { error: 'User was not created.' }
-            }
-
-            // SIgn In the current User 
-            let res = await this.signIn(userData.email, userData.password)
-
-            return {
-                user: res.user,
-                token: res.token
-            }
-
-        } catch (error) {
-            return { error: error }
-        }
-=======
         })
->>>>>>> promise
     }
 }
 
